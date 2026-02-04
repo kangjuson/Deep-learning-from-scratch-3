d5fcbf34e73f6646eaa9a4379d7e61f7bffff790
Juson Kang <kjs200366@gmail.com>
2026-02-04 15:21:25 +0900

    edit step06.py

--- steps/step06.py
+++ steps/step06.py
@@ -6,7 +6,7 @@ class Variable:
         self.grad = None
 
 class Function:
-    def __call__(self, intput):
+    def __call__(self, input):
         x = input.data
         y = self.forward(x)
         output = Variable(y)
@@ -38,3 +38,18 @@ class Exp(Function):
         x = self.input.data
         gx = np.exp(x) * gy
         return gx
+    
+A = Square()
+B = Exp()
+C = Square()
+
+x = Variable(np.array(0.5))
+a = A(x)
+b = B(a)
+y = C(b)
+
+y.grad = np.array(1.0)
+b.grad = C.backward(y.grad)
+a.grad = B.backward(b.grad)
+x.grad = A.backward(a.grad)
+print(x.grad) 
