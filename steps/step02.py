7006b01878b960324bc79ee668484c5f83e9fab8
Juson Kang <kjs200366@gmail.com>
2026-02-04 14:26:37 +0900

    create step03.py

--- steps/step02.py
+++ steps/step02.py
@@ -4,12 +4,19 @@ from step01 import Variable
 class Function:
     def __call__(self, input):
         x = input.data
-        y = x ** 2
+        y = self.forward(x)
         output = Variable(y)
         return output
     
+    def forward(self, x):
+        raise NotImplementedError()
+    
+class Square(Function):
+    def forward(self, x):
+        return x ** 2
+    
 x = Variable(np.array(10))
-f = Function()
+f = Square()
 y = f(x)
 
 print(type(y))
---
+++ steps/step03.py
@@ -0,0 +1,16 @@
+import numpy as np
+from step02 import Function, Square
+from step01 import Variable
+class Exp(Function):
+    def forward(self, x):
+        return np.exp(x)
+    
+A = Square()
+B = Exp()
+C = Square()
+
+x = Variable(np.array(0.5))
+a = A(x)
+b = B(a)
+y = C(b)
+print(y.data)
