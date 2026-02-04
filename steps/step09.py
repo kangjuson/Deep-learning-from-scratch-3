f0f1a8d9ebbcab07bf3ef2f164b352a02e888ce1
Juson Kang <kjs200366@gmail.com>
2026-02-04 17:00:22 +0900

    create step09.py

---
+++ steps/step09.py
@@ -0,0 +1,66 @@
+import numpy as np
+from step08 import Square, Exp
+
+class Variable:
+    def __init__(self, data):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError('{} is not supported'.format(type(data)))
+            
+        self.data = data
+        self.grad = None
+        self.creator = None
+
+    def set_creator(self, func):
+        self.creator = func
+
+    def backward(self):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
+        funcs = [self.creator]
+        while funcs:
+            f = funcs.pop()
+            x, y = f.input, f.output
+            x.grad = f.backward(y.grad)
+
+            if x.creator is not None:
+                funcs.append(x.creator)
+
+class Function:
+    def __call__(self, input):
+        x = input.data
+        y = self.forward(x)
+        output = Variable(as_array(y))
+        output.set_creator(self)
+        self.input = input
+        self.output = output
+        return output
+    
+    def forward(self, x):
+        raise NotImplementedError()
+
+    def backward(self, gy):
+        raise NotImplementedError()
+
+def square(x):
+    return Square()(x)
+
+def exp(x):
+    return Exp()(x)
+
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
+
+x = Variable(np.array(0.5))
+# a = square(x)
+# b = exp(a)
+# y = square(b)
+
+y = square(exp(square(x)))
+
+y.grad = np.array(1.0)
+y.backward()
+print(x.grad)
