b9b38ba33af607984cea015ed4961ac746726c73
Juson Kang <kjs200366@gmail.com>
2026-02-05 17:13:26 +0900

    edit step13.py

--- steps/step13.py
+++ steps/step13.py
@@ -1,5 +1,5 @@
-from step12 import Function
 import numpy as np
+
 
 class Variable:
     def __init__(self, data):
@@ -29,14 +29,52 @@ class Variable:
             for x, gx in zip(f.inputs, gxs):
                 x.grad = gx
 
-            if x.creator is not None:
-                funcs.append(x.creator)
+                if x.creator is not None:
+                    funcs.append(x.creator)
 
 
 def as_array(x):
     if np.isscalar(x):
         return np.array(x)
     return x
+
+
+class Function:
+    def __call__(self, *inputs):
+        xs = [x.data for x in inputs]
+        ys = self.forward(*xs)
+        if not isinstance(ys, tuple):
+            ys = (ys,)
+        outputs = [Variable(as_array(y)) for y in ys]
+
+        for output in outputs:
+            output.set_creator(self)
+        self.inputs = inputs
+        self.outputs = outputs
+        return outputs if len(outputs) > 1 else outputs[0]
+
+    def forward(self, xs):
+        raise NotImplementedError()
+
+    def backward(self, gys):
+        raise NotImplementedError()
+
+
+class Square(Function):
+    def forward(self, x):
+        y = x ** 2
+        return y
+
+    def backward(self, gy):
+        x = self.inputs[0].data
+        gx = 2 * x * gy
+        return gx
+    
+
+def square(x):
+    f = Square()
+    return f(x)
+
 
 class Add(Function):
     def forward(self, x0, x1):
@@ -45,4 +83,17 @@ class Add(Function):
     
     def backward(self, gy):
         return gy, gy
-    
+
+
+def add(x0, x1):
+    return Add()(x0, x1)
+
+
+x = Variable(np.array(2.0))
+y = Variable(np.array(3.0))
+
+z = add(square(x), square(y))
+z.backward()
+print(z.data)
+print(x.grad)
+print(y.grad)
