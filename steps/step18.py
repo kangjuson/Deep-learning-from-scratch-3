e2a8ca018cc2d6e906c6e336ce8ca2ccd16ed807
Juson Kang <kjs200366@gmail.com>
2026-02-07 19:04:40 +0900

    create step18.py

---
+++ steps/step18.py
@@ -0,0 +1,152 @@
+import weakref
+import contextlib
+import numpy as np
+
+class Config:
+    enable_backprop = True
+
+class Variable:
+    def __init__(self, data):
+        if data is None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError('{} is not supported'.format(type(data)))
+            
+        self.data = data
+        self.grad = None
+        self.creator = None
+        self.generation = 0 
+
+    def set_creator(self, func):
+        self.creator = func
+        self.generation = func.generation + 1
+
+    def backward(self, retain_grad=False):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
+        funcs = []
+        seen_set = set()
+        def add_func(f):
+            if f not in seen_set:
+                funcs.append(f)
+                seen_set.add(f)
+                funcs.sort(key=lambda x: x.generation)
+
+        add_func(self.creator)
+
+        while funcs:
+            f = funcs.pop()
+            gys = [output().grad for output in f.outputs]
+            gxs = f.backward(*gys)
+            if not isinstance(gxs, tuple):
+                gxs = (gxs,)
+
+            for x, gx in zip(f.inputs, gxs):
+                if x.grad is None:
+                    x.grad = gx
+                else:
+                    x.grad = x.grad + gx
+
+                if x.creator is not None:
+                    add_func(x.creator)
+                    
+            if not retain_grad:
+                for y in f.outputs:
+                    y().grad = None
+
+    def cleargrad(self):
+        self.grad = None
+
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
+
+class Function(object):
+    def __call__(self, *inputs):
+        xs = [x.data for x in inputs]
+        ys = self.forward(*xs)
+        if not isinstance(ys, tuple):
+            ys = (ys,)
+        outputs = [Variable(as_array(y)) for y in ys]
+
+        if Config.enable_backprop:
+            self.generation = max([x.generation for x in inputs]) 
+            for output in outputs:
+                output.set_creator(self)
+            self.inputs = inputs
+            self.outputs = [weakref.ref(output) for output in outputs]
+            self.generation = max([x.generation for x in inputs])
+
+        return outputs if len(outputs) > 1 else outputs[0]
+
+    def forward(self, xs):
+        raise NotImplementedError()
+    
+    def backward(self, gys):
+        raise NotImplementedError()
+    
+class Square(Function):
+    def forward(self, x):
+        y = x ** 2
+        return y
+
+    def backward(self, gy):
+        x = self.inputs[0].data
+        gx = 2 * x * gy
+        return gx
+    
+def square(x):
+    f = Square()
+    return f(x)
+
+class Add(Function):
+    def forward(self, x0, x1):
+        y = x0 + x1
+        return y
+    
+    def backward(self, gy):
+        return gy, gy
+
+
+def add(x0, x1):
+    return Add()(x0, x1)
+
+@contextlib.contextmanager
+def config_test(name, value):
+    old_value = getattr(Config, name)
+    setattr(Config, name, value)
+    try:
+        yield
+    finally:
+        setattr(Config, name, old_value)
+
+def no_grad():
+    return config_test('enable_backprop', False)
+
+with config_test('enable_backprop', False):
+    x = Variable(np.array(2.0))
+    y = square(x)
+
+with no_grad():
+    x = Variable(np.array(2.0))
+    y = square(x)
+# x0 = Variable(np.array(1.0))
+# x1 = Variable(np.array(1.0))
+# t = add(x0, x1)
+# y = add(x0, t)
+# y.backward()
+
+# print(y.grad, t.grad)
+# print(x0.grad, x1.grad)
+
+# Config.enable_backprop = True
+# x = Variable(np.ones((100, 100, 100)))
+# y = square(square(square(x)))
+# y.backward()
+
+# Config.enable_backprop = False
+# x = Variable(np.ones((100, 100, 100)))
+# y = square(square(square(x)))
+
+# print(y.grad, y.grad)
